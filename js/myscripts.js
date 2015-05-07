34e4a0845ee3af9bddb2b7e9bf7b08cbdba7f092
Maharsh Patel <harryainlaysc@gmail.com>
2015-05-07 10:50:14 -0600

    Update myscripts.js

--- js/myscripts.js
+++ js/myscripts.js
@@ -17,8 +17,8 @@ var formsList = [
     ["Grade 12 Rep", "April 22", "https://drive.google.com/uc?export=download&id=0B7yOvdmsLVw5Vm1OM0E0aUF1ZEZoVlo2VDlSWkNTSG1vRjU0"]
 ];
 
-var studentCouncilApplicationOpenTRUEorFALSE = True;
-var studentCouncilApplicationFormLocation = "https://drive.google.com/file/d/0B1cysqXxoPvxLTFuQjRkWEZITVU/view?usp=sharing";
+var studentCouncilApplicationOpenTRUEorFALSE = False;
+var studentCouncilApplicationFormLocation = "Use this to place the link to file.";
 
 var navigationBarColour = "#222222"             //DEFAULT = #222222
 
